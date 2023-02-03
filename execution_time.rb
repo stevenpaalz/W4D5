8da172a4a658d6db9e7d6e6196f4bc6a642c1f49
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-03 13:43:41 -0500

    fast and slow min

--- execution_time.rb
+++ execution_time.rb
@@ -1,13 +1,27 @@
 def my_min_slow(array)
-    min = array[0]
-    (1...array.length).each do |i|
-        (2...array.length).each do |e|
-            if array[i] > array[e] && array[e] < min
-                min = array[e] 
-            else
-                min = array[i]
-            end
-        end
+  min = array[0]
+  (0...array.length).each do |i|
+    (i + 1...array.length).each do |e|
+      if (array[i] > array[e] && array[e] < min)
+        min = array[e]
+      elsif (array[i] < array[e] && array[i] < min)
+        min = array[i]
+      end
     end
-    min
+  end
+  min
 end
+# O(n^2)
+
+def my_min_fast(array)
+  min = array[0]
+  (1...array.length).each do |i|
+    if min > array[i]
+      min = array[i]
+    end
+  end
+  min
+end
+# O(n)
+
+
